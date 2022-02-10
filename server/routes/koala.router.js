83f2d0210879b0c365d052f089c9146d70526985
Mark Terry <mark.terry93@gmail.com>
2022-02-10 16:32:58 -0600

    server side POST all done

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -30,12 +30,22 @@ koalaRouter.post('/', (req, res) => {
     console.log(newKoala);
     
     const queryText = `
-    INSERT INTO "allKoalas"
+    INSERT INTO "allKoalas" ("name", "age", "gender", "transfer", "notes")
+    VALUES ($1, $2, $3, $4, $5);
     `;
 
+        //parameterized query below, prevents SQL injection
+    pool.query(queryText, [newKoala.name, newKoala.age, newKoala.gender, newKoala.transfer, newKoala.notes])
+    .then((result) => {
+        res.sendStatus(201);
+    })
+    .catch((err) => {
+        console.log('error querying', queryText, err);
+        res.sendStatus(500);
+    })
     // console.log(req.body); --- original code
 
-    res.sendStatus(200)
+    // res.sendStatus(200)
 })
 
 
