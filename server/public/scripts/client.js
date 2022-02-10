3db255a4362f677b0e96b971940ff50047db4abd
Mark Terry <mark.terry93@gmail.com>
2022-02-10 14:08:58 -0600

    postKoala() input values done

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -40,16 +40,33 @@ function saveKoala( newKoala ){
 }
 
 
-function addKoala(koalaToAdd) {
+
+
+function postKoala() {
+  let koalaToAdd = {
+    name: $('#nameIn').val(),
+    age: $('#ageIn').val(),
+    gender: $('#genderIn').val(),
+    transfer: $('#readyForTransferIn').val(),
+    notes: $('#notesIn').val()
+  }
   $.ajax({
     type: 'POST',
     url: '/koalas',
     data: koalaToAdd,
     }).then(function(response) {
       console.log('Response from server.', response);
+      //empty inputs
+      $('#nameIn').val(''),
+      $('#ageIn').val(''),
+      $('#genderIn').val(''),
+      $('#readyForTransferIn').val(''),
+      $('#notesIn').val('')
+
       // append to DOM with a function here
+
     }).catch(function(error) {
-      console.log('Error in POST addKoala()', error)
+      console.log('Error in POST postKoala()', error)
       alert('Unable to add koala at this time. Please try again later.');
     });
-}// end addKoala
+}// end postKoala
